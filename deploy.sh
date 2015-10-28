e5ca0bc947f558143a388c16744267882cb5a0ad
Sergey Cherepanov <scher56@gmail.com>
2015-10-28 12:27:46 +0000

    deploy.sh edited online with Bitbucket

--- deploy.sh
+++ deploy.sh
@@ -5,12 +5,12 @@ PYTHON=`which python`
 test -x ./.env/bin/python && PYTHON=./.env/bin/python
 echo "Selected $PYTHON executable"
 
+echo "Removing all .pyc files..."
 find ./project -name '*.pyc' -delete
-echo "Removed all .pyc files"
 
 $PYTHON manage.py collectstatic --noinput
 $PYTHON manage.py migrate
-$PYTHON manage.py compress
+# $PYTHON manage.py compress
 
-supervisorctl restart turkmen
+supervisorctl restart PROJECT_NAME
 
