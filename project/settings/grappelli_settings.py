93260ccc746ef0887a9bea7d904998822a9b85db
Sergey Cherepanov <scher56@gmail.com>
2015-11-04 09:13:40 +0000

    grappelli_settings.py edited online with Bitbucket

--- project/settings/grappelli_settings.py
+++ project/settings/grappelli_settings.py
@@ -1,4 +1,4 @@
 # -*- coding: utf-8 -*-
 
-GRAPPELLI_ADMIN_TITLE = 'Mockingbird Admin Panel'
+GRAPPELLI_ADMIN_TITLE = 'Project Admin Panel'
 # GRAPPELLI_INDEX_DASHBOARD = 'project.settings.dashboard.CustomIndexDashboard'
