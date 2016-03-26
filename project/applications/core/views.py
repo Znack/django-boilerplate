7c22229e9a7a3208b8f87e336cce3669b56b07df
znack <scher56@gmail.com>
2016-03-26 14:17:31 +0600

    add xml to reqs and fix imports in core.views

--- project/applications/core/views.py
+++ project/applications/core/views.py
@@ -1,14 +1,7 @@
-from rest_framework import views, viewsets
+from rest_framework import viewsets
 from rest_framework.response import Response
 from rest_framework_xml.renderers import XMLRenderer
-from rest_framework import status
-from django.http import HttpResponse
-from django.conf import settings
-import random
-import psutil
-import platform
-from models import Image
-from serializers import SystemInfoSerializer, SamplePayloadSerializer
+from serializers import SamplePayloadSerializer
 
 
 class BaseTextPayloadViewSet(viewsets.ViewSet):
