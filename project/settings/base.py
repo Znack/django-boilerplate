66442f76f4d0605fd3e7e1129f1c4305e73353a4
Sergey Cherepanov <scher56@gmail.com>
2016-04-14 08:06:43 +0000

    base.py edited online with Bitbucket

--- project/settings/base.py
+++ project/settings/base.py
@@ -14,47 +14,21 @@ import sys
 from project.exceptions import ProjectBaseDirectoryVariableException
 from django.utils.translation import ugettext_lazy as _
 
-# base repo folder, here stored virtualenv files, manage.py etc
-try:
-    BASE_DIR = os.environ['PROJECT_PATH']
-except KeyError as error:
-    if os.path.isdir("/opt/python/current/app/"):
-        # we are on AWS Beanstalk
-        BASE_DIR = os.path.abspath("/opt/python/current/app/")
-    else:
-        raise ProjectBaseDirectoryVariableException(
-            "You have no set env variable PROJECT_PATH, it should store a reference to project directory. "
-            "Maybe you haven't used the manage.py file for launch project? "
-            "Or set PROJECT_PATH manually, it must contain absolute path to project main directory with manage.py file."
-        )
-
-# folder with collected static files, ready to download by client apps
-PUBLIC_ROOT = os.path.join(BASE_DIR, "public")
 
 # folder with main django project which contains settings, urls and applications package
 PROJECT_ROOT = os.path.abspath(os.path.join(os.path.dirname(__file__), os.pardir))
 
+# base repo folder, here stored virtualenv files, manage.py etc
+BASE_DIR = os.path.abspath(os.path.join(PROJECT_ROOT, os.pardir))
+
+# folder with collected static files, ready to download by client apps
+PUBLIC_ROOT = os.path.join(BASE_DIR, "public")
+
 sys.path.append(os.path.join(PROJECT_ROOT, 'applications'))
-
-
-# Application definition
-
-INSTALLED_APPS = (
-    'django.contrib.admin',
-    'django.contrib.auth',
-    'django.contrib.contenttypes',
-    'django.contrib.sessions',
-    'django.contrib.messages',
-    'django.contrib.staticfiles',
-    'core',
-)
 
 
 ROOT_URLCONF = 'project.urls'
 
 WSGI_APPLICATION = 'project.wsgi.application'
-
-
-
 
 
